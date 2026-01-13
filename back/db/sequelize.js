52335edc7a1e6b41527923ff2ec7c59c17d77b09
gonza laptop <gonz.r@hotmail.com>
2026-01-13 08:12:19 -0300

    database setup

---
+++ back/db/sequelize.js
@@ -0,0 +1,27 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const sequelize = new sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.HOST,
+        port: process.env.PORT ?? 3306,
+        dialect: 'mysql',
+        logging: false 
+    }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log("Conexión a MySQL establecida correctamente.");
+  } catch (error) {
+    console.error("Error al conectar con MySQL:", error);
+    }
+};
+
+export { sequelize, connectDB };
---
+++ back/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const app = express();
+app.disable('X-Powered-by');
+const port = process.env.PORT || 3000;
+import path from 'path';
+import cors from 'cors';
+//Routers
+
+
+import { connectDB } from "./db/sequelize.js";
+connectDB();
+
+app.use(cors({
+    origin: [
+        //frontend URLS
+    process.env.FRONTEND_URL1,
+    process.env.FRONTEND_URL2
+        //localhost
+        //ip
+    ],
+    // investigar esto
+    methods: ["GET", "POST", "PUT", "DELETE"]
+    //credentials: true,
+}));
+app.use(express.json());
